89b5db250e4784526eb5586352f3f6aee520a6b4
Law-son <buabassahlawson@gmail.com>
2024-10-09 16:43:26 +0000

    fix(ui): make footer responsive

--- schedo-frontend/src/components/footer/Footer.jsx
+++ schedo-frontend/src/components/footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
           <span className="self-center whitespace-nowrap text-2xl font-semibold text-primary-blue md:text-4xl lg:text-3xl">
             Schedo
           </span>
-          <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+          <ul class="hidden sm:flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
               <a href="#" class="hover:underline me-4 md:me-6">
                 Home
